08f3cbd28891631d9dd7ab6876d7acbbc209c195
InhoChoi <revaer@naver.com>
2016-05-18 18:10:50 +0900

    getNowDateString() - 버그 수정

--- src/worker.js
+++ src/worker.js
@@ -25,9 +25,9 @@ export default async()=>{
 function getNowDateString(){
   const now = new Date();
   const year = now.getFullYear();
-  const month = now.getMonth() > 10 ? now.getMonth() : '0' + now.getMonth();
-  const date = now.getDate() > 10 ? now.getDate() : '0' + now.getDate();
-  const hour = now.getHours() > 10 ? now.getHours() : '0' + now.getHours();
-  const min = now.getMinutes() > 10 ? now.getMinutes() : '0' + now.getMinutes();
+  const month = now.getMonth() >= 10 ? now.getMonth() : '0' + now.getMonth();
+  const date = now.getDate() >= 10 ? now.getDate() : '0' + now.getDate();
+  const hour = now.getHours() >= 10 ? now.getHours() : '0' + now.getHours();
+  const min = now.getMinutes() >= 10 ? now.getMinutes() : '0' + now.getMinutes();
   return `${year}-${month}-${date} ${hour}:${min}`;
 }
