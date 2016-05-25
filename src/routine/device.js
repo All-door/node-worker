66dd32f7fef9ee472c7f5b5866e70c269024a0f2
InhoChoi <revaer@naver.com>
2016-05-25 14:34:11 +0900

    UTIL: Device Routine 문자 서비스 구현 완료

--- src/routine/device.js
+++ src/routine/device.js
@@ -1,12 +1,12 @@
 import { GetDeviceInfoByRoom } from '../db/redis'
 import { GetUserByID, GetUsers } from '../db/mongo'
-import SMS from '../util/sms'
+import { sendSMS } from '../util/sms'
 
 export default async(rooms)=>{
   for(let i=0,len=rooms.length;i<len;i++){
     let status = await GetDeviceInfoByRoom(rooms[i]);
     let user = await GetUserByID(rooms[i].user_id);
-    let phoneNumber = user.phoneNumber ? user.phoneNumber : null;
+    let phoneNumber = user.phoneNumber ? user.phoneNumber.replace(new RegExp('-','g'),'') : null;
 
     if(isValidStatus(status)){
       status = JSON.parse(status);
@@ -14,23 +14,21 @@ export default async(rooms)=>{
       const updatedAt = getUpdatedAtFromStatus(status);
       const diff = getDiffMinuteFromNow(updatedAt);
 
-      if( diff >= 30 && diff < 60){ // 30분 이상
-        console.log("[ " + rooms[i].title + " / " + rooms[i].device_id + " ] - All-Door 디바이스가 30분 이상 연결이 되지 않았습니다. " + phoneNumber);
-      }
-      else if( diff >= 120 && diff < 150){ //2시간 이상
-        console.log("[ " + rooms[i].title + " / " + rooms[i].device_id + " ] - All-Door 디바이스가 2시간 이상 연결이 되지 않았습니다. " + phoneNumber);
+      if( diff >= 60 && diff < 90){ //1시간 이상
+        console.log("[ " + rooms[i].title + " / " + rooms[i].device_id + " ] - All-Door 디바이스가 1시간 이상 연결이 되지 않았습니다. All-Door Service to " + phoneNumber);
+        await sendSMS(phoneNumber,`[${getRoomTitle(rooms[i])}] 디바이스가 1시간 이상 연결이 되지 않았습니다. 연결 상태를 확인해주세요. All-Door Service`);
       }
       else if( diff >= 600 && diff < 630){ // 10시간 이상
-        console.log("[ " + rooms[i].title + " / " + rooms[i].device_id + " ] - All-Door 디바이스가 10시간 이상 연결이 되지 않았습니다. " + phoneNumber);
+        console.log("[ " + rooms[i].title + " / " + rooms[i].device_id + " ] - All-Door 디바이스가 10시간 이상 연결이 되지 않았습니다. All-Door Service to " + phoneNumber);
+        await sendSMS(phoneNumber,`[${getRoomTitle(rooms[i])}] 디바이스가 10시간 이상 연결이 되지 않았습니다. 연결 상태를 확인해주세요. All-Door Service`);
       }
-      else if( diff >= 720 && isNowHour(9)){ // 12 시간 이상 - 아침 9시 알람
-        console.log("[ " + rooms[i].title + " / " + rooms[i].device_id + " ] - All-Door 디바이스가 12시간 이상 연결이 되지 않았습니다. " + phoneNumber);
+      else if( diff >= 24*60 && isNowHour(9)){ // 하루 이상 - 저녁 8시 알람
+        console.log("[ " + rooms[i].title + " / " + rooms[i].device_id + " ] - All-Door 디바이스가 하루 이상 연결이 되지 않았습니다. All-Door Service to " + phoneNumber);
+        await sendSMS(phoneNumber,`[${getRoomTitle(rooms[i])}] 디바이스가 하루 이상 연결이 되지 않았습니다. 연결 상태를 확인해주세요. All-Door Service`);
       }
-      else if( diff >= 720 && isNowHour(20)){ // 12시간 이상 - 저녁 8시 알람
-        console.log("[ " + rooms[i].title + " / " + rooms[i].device_id + " ] - All-Door 디바이스가 12시간 이상 연결이 되지 않았습니다. " + phoneNumber);
-      }
-    }else if(isNowHour(9) || isNowHour(20)){
-      console.log("[ " + rooms[i].title + " / " + rooms[i].device_id + " ] - All-Door 디바이스가 서버에 연결이 되지 않았습니다. " + phoneNumber);
+    }else if(isNowHour(9)){ // 인터넷 연결 문제
+      console.log("[ " + rooms[i].title + " / " + rooms[i].device_id + " ] - All-Door 디바이스가 서버 연결이 되지 않았습니다. All-Door Service to " + phoneNumber);
+      await sendSMS(phoneNumber,`[${getRoomTitle(rooms[i])}] 디바이스가 서버 연결이 되지 않았습니다. 연결 상태를 확인해주세요. All-Door Service`);
     }
   }
 };
@@ -52,4 +50,12 @@ function getDiffMinuteFromNow(updatedAt){
 function isNowHour(hour){
   const now = new Date();
   return now.getHours() == hour && now.getMinutes() > 0 && now.getMinutes() <= 30;
+}
+
+function getRoomTitle(room){
+  if( room.title.length > 10){
+    return room.title.substring(0,10)+"...";
+  }else {
+    return room.title;
+  }
 }
